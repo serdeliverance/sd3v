5ab2857ff695ec36b3a26abdb276b0283cdde4e7
Sergio Cano <canosergio90@gmail.com>
2022-11-08 11:28:52 +0100

    creating basic pages and adding material ui
    Signed-off-by: Sergio Cano <canosergio90@gmail.com>

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,11 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+// where should I place this
+import '@fontsource/roboto/300.css'
+import '@fontsource/roboto/400.css'
+import '@fontsource/roboto/500.css'
+import '@fontsource/roboto/700.css'
 
 const Home: NextPage = () => {
   return (
